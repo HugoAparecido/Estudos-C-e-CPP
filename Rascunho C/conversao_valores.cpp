b7e93bdbea7b119939aeed2355fcb5999f6beae3
Hugo Aparecido Gregorio de Almeida <hugoapga626@gmail.com>
2025-04-27 18:12:01 -0300

    começo rascunho

---
+++ Rascunho C/conversao_valores.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int entrada;
+    cin >> entrada;
+    // Agora a variável 'entrada' contém o número inteiro que o usuário digitou.
+    // Você pode usar essa variável para outras operações aqui.
+
+    return 0;
+}
